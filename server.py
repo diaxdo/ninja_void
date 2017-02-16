dca28010aa0d51c12a277aa5565b1571978a20a3
Dalia <dalia.icedo@gmail.com>
2017-02-16 01:02:23 -0800

    Added host localhost to server.py

--- server.py
+++ server.py
@@ -162,4 +162,4 @@ def comment(message_id):
     return redirect('/wall/{}'.format(message_id))
 
 port = int(os.environ.get('PORT', 5000))
-app.run(debug=True, port=port)
+app.run(debug=True, host='0.0.0.0', port=port)
